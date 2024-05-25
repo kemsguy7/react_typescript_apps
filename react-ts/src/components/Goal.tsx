fe55e97aad143367344dee7647f0ae9eee353fb7
Matthew Idungafa <mattidungafa@gmail.com>
2024-05-25 19:46:33 +0100

    Started goal component

---
+++ react-ts/src/components/Goal.tsx
@@ -0,0 +1,11 @@
+export default function Goal() {
+  return (
+    <article>
+      <div>
+        <h2> TITLE </h2>
+        <p>DESCRIPTION</p>
+      </div>
+      <button>Delete</button>
+    </article>
+  )
+}
