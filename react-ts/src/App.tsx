c0906613b248273485661dffee68af2102fb4bfa
Matthew Idungafa <mattidungafa@gmail.com>
2024-05-26 09:46:44 +0100

    updates with installation

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Goal from './components/Goal'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  
 export default function App() {
-  return <h1>Let's get started </h1>
+  return (
+    <main>
+      <Goal />
+    </main>
+  )
 }
