987a828f0dc8a8e0e1603ed63f1ee02ad25fc973
Matthew Idungafa <mattidungafa@gmail.com>
2024-05-26 11:40:06 +0100

    using functional components

--- react-ts/src/components/Goal.tsx
+++ react-ts/src/components/Goal.tsx
@@ -1,11 +1,26 @@
-import { type PropsWithChildren, ReactNode } from 'react'
+import { type FC, type PropsWithChildren, ReactNode } from 'react'
 
 // interface GoalProps {
 //   title: string
 //   children: ReactNode
 // }
 
-// Alternative approach to the code above using a generic typescript function
+//Alternative way using functional components
+// const Goal: FC<GoalProps> = ({ title, children }) => {
+//   return (
+//     <article>
+//       <div>
+//         <h2> {title} </h2>
+//         <p> {children} </p>
+//       </div>
+//       <button>Delete</button>
+//     </article>
+//   )
+// }
+
+//export default Goal
+
+// Alternative approach to the codes above above using a generic typescript function
 
 type GoalProps = PropsWithChildren<{ title: string }> //generic type
 
