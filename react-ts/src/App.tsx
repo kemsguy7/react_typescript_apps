76862948909a3b5507d2d4212f60a5c088a315f5
Matthew Idungafa <mattidungafa@gmail.com>
2024-05-26 11:25:00 +0100

    recieving children props

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Goal from './components/Goal'
 
 export default function App() {
   return (
     <main>
-      <Goal />
+      <Goal title="React + TS">
+        <p>
+          <em> Learn it from the ground up! </em>{' '}
+        </p>
+      </Goal>
     </main>
   )
 }
--- react-ts/src/components/Goal.tsx
+++ react-ts/src/components/Goal.tsx
@@ -1,9 +1,20 @@
-export default function Goal() {
+import { type PropsWithChildren, ReactNode } from 'react'
+
+// interface GoalProps {
+//   title: string
+//   children: ReactNode
+// }
+
+// Alternative approach to the code above using a generic typescript function
+
+type GoalProps = PropsWithChildren<{ title: string }> //generic type
+
+export default function Goal({ title, children }: GoalProps) {
   return (
     <article>
       <div>
-        <h2> TITLE </h2>
-        <p>DESCRIPTION</p>
+        <h2> {title} </h2>
+        <p> {children} </p>
       </div>
       <button>Delete</button>
     </article>
