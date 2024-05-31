5549749678f2ac3a87ae2c3c40be8685625f0c5d
Matthew Idungafa <mattidungafa@gmail.com>
2024-05-31 05:56:37 +0100

    adding states

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -3,13 +3,29 @@ import Goal from './components/Goal'
 import Header from './components/Header'
 import goalsImg from './assets/goals.jpeg'
 
+type Goal = {
+  title: string;
+  description: string;
+  id: number;
+}
+
 
 export default function App() {
+
+    const [goals, setGoals] = useState<>([]);
+
+    function handleAddGoal() {
+      console.log('Add Goal')
+    }
+
+
   return (
+
     <main>
       <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
         <h1> My Course Goals</h1>
       </Header>
+      <button>Add Goal </button>
       <Goal title="React + TS">
         <p>
           <em> Learn it from the ground up! </em>
