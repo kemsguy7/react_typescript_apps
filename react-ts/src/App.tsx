bea019a9d72aab63779eddaddb9df66180483581
Matthew Idungafa <mattidungafa@gmail.com>
2024-06-01 14:37:35 +0100

    updates

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -4,33 +4,42 @@ import Header from './components/Header'
 import goalsImg from './assets/goals.jpeg'
 
 type Goal = {
-  title: string;
-  description: string;
-  id: number;
+  title: string
+  description: string
+  id: number
 }
 
-
 export default function App() {
+  const [goals, setGoals] = useState<Goal[]>([])
 
-    const [goals, setGoals] = useState<Goal[]>([]);
-
-    function handleAddGoal() {
-      console.log('Add Goal')
-    }
-
+  function handleAddGoal() {
+    setGoals((prevGoals) => {
+      const newGoal: Goal = {
+        id: Math.random(),
+        title: 'Learn React + TS',
+        description: 'Learn it in depth!',
+      }
+      return [...prevGoals, newGoal]
+    })
+  }
 
   return (
-
     <main>
       <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
         <h1> My Course Goals</h1>
       </Header>
-      <button>Add Goal </button>
-      <Goal title="React + TS">
-        <p>
-          <em> Learn it from the ground up! </em>
-        </p>
-      </Goal>
+      <button onClick={handleAddGoal}>Add Goal </button>
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <Goal title="React + TS">
+              <p>
+                <em> Learn it from the ground up! </em>
+              </p>
+            </Goal>
+          </li>
+        ))}
+      </ul>
     </main>
   )
 }
