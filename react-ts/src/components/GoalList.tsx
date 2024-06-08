1df5d9b13a105b93f9cc8cf80d4f68a51b4ef7be
Matthew Idungafa <mattidungafa@gmail.com>
2024-06-08 18:45:29 +0100

    updates

--- react-ts/src/components/GoalList.tsx
+++ react-ts/src/components/GoalList.tsx
@@ -1,5 +1,7 @@
+import { type ReactNode } from 'react'
 import Goal from './Goal'
 import { Goal as CGoal } from '../App'
+import InfoBox from './InfoBox'
 
 type GoalListProps = {
   goals: CGoal[] //goals prop inherits type from App.ts
@@ -7,15 +9,36 @@ type GoalListProps = {
 }
 
 export default function GoalList({ goals, onDeleteGoal }: GoalListProps) {
+  //check if goal is empty
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">
+        You have no course goals yet. Start adding some!{' '}
+      </InfoBox>
+    )
+  }
+  //conditionally show th infobox if we have too many goals
+  let warningBox: ReactNode
+
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning">
+        You're collecting a lot of goals. Don't put too much on your plate!
+      </InfoBox>
+    )
+  }
+
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <Goal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
-            <p>{goal.description}</p>
-          </Goal>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <Goal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
+              <p>{goal.description}</p>
+            </Goal>
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
