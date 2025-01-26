87b520eb13f1d53b8e1b5a8cf7ead9c4d5fb415d
kemsguy7 <mattidungafa@gmail.com>
2025-01-26 17:48:47 +0100

    updates: started the app Context files

---
+++ states/src/AppContext.tsx
@@ -0,0 +1,50 @@
+import { createContext, useContext, useReducer, ReactNode, } from 'react';
+import { User } from './api/authenticate';
+
+
+
+type State = {
+    user: undefined | User;
+    permissions: undefined | string[];
+    loading: boolean;
+  };
+  const initialState: State = {
+    user: undefined,
+    permissions: undefined,
+    loading: false,
+  };
+  
+  type Action =
+    | {
+        type: 'authenticate';
+      }
+    | {
+        type: 'authenticated';
+        user: User | undefined;
+      }
+    | {
+        type: 'authorize';
+      }
+    | {
+        type: 'authorized';
+        permissions: string[];
+      };
+  
+  function reducer(state: State, action: Action): State {
+    switch (action.type) {
+      case 'authenticate':
+        return { ...state, loading: true };
+      case 'authenticated':
+        return { ...state, loading: false, user: action.user };
+      case 'authorize':
+        return { ...state, loading: true };
+      case 'authorized':
+        return {
+          ...state,
+          loading: false,
+          permissions: action.permissions,
+        };
+      default:
+        return state;
+    }
+  }
