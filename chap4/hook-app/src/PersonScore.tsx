18d9ed80e141ef12c560a8ff239e939b33ce0792
kemsguy7 <mattidungafa@gmail.com>
2025-03-06 17:07:56 +0100

    updates: simple useState counter

--- chap4/hook-app/src/PersonScore.tsx
+++ chap4/hook-app/src/PersonScore.tsx
@@ -1,9 +1,50 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getPerson } from './getPerson';
 
 export function PersonScore() {
+  const [name, setName] = useState<string | undefined>();
+  const [score, setScore] = useState(0);
+  const [loading, setLoading] = useState(true);
+
+  // useEffect(() => {
+  //   getPerson().then((person) => console.log(person));
+  // }, []); // first effect using promise
+
   useEffect(() => {
     getPerson().then((person) => console.log(person));
   }, []);
-  return null;
+
+  useEffect(() => {
+    getPerson().then((person) => {
+      //
+      setLoading(false);
+      setName(person.name);
+    });
+  }, []);
+
+  //  display a loading indicator while the loading state is set to true
+  if (loading) {
+    return <div> Loading... </div>;
+  }
+
+  return (
+    <div>
+      <h3>
+        {' '}
+        {name}, {score}
+      </h3>
+      <button onClick={() => setScore(score + 1)}>Add</button>
+      <button onClick={() => setScore(score - 1)}>Subtract</button>
+      <button onClick={() => setScore(0)}> Reset </button>
+    </div>
+  );
+
+  //Effect using async await
+  // useEffect(() => {
+  //   async function getThePerson() {
+  //     const person = await getPerson();
+  //     console.log(person);
+  //   }
+  //   getThePerson();
+  // }, []);
 }
--- chap4/hook-app/src/getPerson.ts
+++ chap4/hook-app/src/getPerson.ts
@@ -1,6 +1,8 @@
 type Person = {
   name: string;
 };
+
+//this function asynchronously returns an objet, { name: "Bob"}, after a second has elapsed
 export function getPerson(): Promise<Person> {
-  return new Promise((resolve) => setTimeout(() => resolve({ name: 'Bob' }), 1000));
+  return new Promise((resolve) => setTimeout(() => resolve({ name: 'Bob' }), 4000));
 }
