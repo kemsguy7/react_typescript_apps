3540b4ee59dfb7e62d9b2a6ca5c9b5e76db0f891
kemsguy7 <mattidungafa@gmail.com>
2025-03-08 02:58:35 +0100

    updates: added reducer function with states and action

--- chap4/hook-app/src/PersonScore.tsx
+++ chap4/hook-app/src/PersonScore.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useReducer } from 'react';
 import { getPerson } from './getPerson';
 
 export function PersonScore() {
@@ -25,6 +25,37 @@ export function PersonScore() {
   //  display a loading indicator while the loading state is set to true
   if (loading) {
     return <div> Loading... </div>;
+  }
+
+  //defining types for useReducer
+  type State = {
+    name: string | undefined;
+    score: number;
+    loading: boolean;
+  };
+
+  //defining types for all the action objects
+  type Action =
+    | { type: 'initialize'; name: string }
+    | { type: 'increment' }
+    | { type: 'decrement' }
+    | { type: 'reset' };
+
+  //define the type reducer function
+  function reducer(state: State, action: Action): State {
+    switch (action.type) {
+      case 'initialize':
+        return { name: action.name, score: 0, loading: false };
+      case 'increment':
+        return { ...state, score: state.score + 1 };
+      case 'decrement':
+        return { ...state, score: state.score - 1 };
+      case 'reset':
+        return { ...state, score: 0 };
+
+      default:
+        return state;
+    }
   }
 
   return (
